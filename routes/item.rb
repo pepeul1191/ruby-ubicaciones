2a8755904cb11c03467a924f44282b26e7521e51
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-30 12:23:52 -0500

    Listar items de un subtitulo

---
+++ routes/item.rb
@@ -0,0 +1,5 @@
+class MyApp < Sinatra::Base
+    get '/item/listar/:subtitulo_id' do
+        Item.select(:id, :nombre, :url).where(:subtitulo_id => params['subtitulo_id']).all().to_a.to_json
+    end
+end
