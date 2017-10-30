4bb7ac016e1bb01be402397e867941cec8434626
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-30 12:11:47 -0500

    Listar los módulos de un sistema

---
+++ routes/modulo.rb
@@ -0,0 +1,5 @@
+class MyApp < Sinatra::Base
+    get '/modulo/listar/:sistema_id' do
+        Modulo.select(:id, :url, :nombre).where(:sistema_id => params['sistema_id']).all().to_a.to_json
+    end
+end
