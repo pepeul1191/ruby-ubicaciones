f146437f4b47700a7226a8bc8d2117369f579e2c
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-30 12:13:34 -0500

    Listar subtitulos de un modulo

---
+++ routes/subtitulo.rb
@@ -0,0 +1,5 @@
+class MyApp < Sinatra::Base
+    get '/subtitulo/listar/:modulo_id' do
+        Subtitulo.select(:id, :nombre).where(:modulo_id => params['modulo_id']).all().to_a.to_json
+    end
+end
