d0698555cfc1c151008a0a5ff02eecf91046fb6c
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-31 08:08:48 -0500

    Validar usuarios

--- routes/usuario.rb
+++ routes/usuario.rb
@@ -2,4 +2,13 @@ class MyApp < Sinatra::Base
     get '/usuario/listar' do
         Usuario.all.to_a.to_json
     end
+
+    post '/usuario/validar' do
+  		rpta = Usuario.where(:usuario => params['usuario'], :contrasenia => params['contrasenia']).count()
+    	if rpta == 1
+    		usuario_id = Usuario.select(:id).where(:usuario => params['usuario'], :contrasenia => params['contrasenia']).first().id
+			Acceso.new(:usuario_id => usuario_id, :momento => Time.now).save
+    	end
+    	rpta.to_s
+    end
 end
