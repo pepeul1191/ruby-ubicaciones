ac7f93b9a31e5e93c4baff23d4dc3ba0f6784da9
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-31 08:37:15 -0500

    Lista de usuarios y sistemas

--- routes/usuario.rb
+++ routes/usuario.rb
@@ -1,6 +1,8 @@
 class MyApp < Sinatra::Base
     get '/usuario/listar' do
-        Usuario.all.to_a.to_json
+		DB.fetch('
+        	SELECT U.id AS id, U.usuario AS usuario, A.momento AS momento, U.correo AS correo 
+        	FROM usuarios U INNER JOIN accesos A ON U.id = A.usuario_id GROUP BY U.usuario ORDER BY U.id').to_a.to_json
     end
 
     post '/usuario/validar' do
