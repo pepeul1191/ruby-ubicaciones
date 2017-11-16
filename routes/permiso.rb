8b4fe500846e8615d3a7d4c9afc32a1b4f6e14a1
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-16 00:19:40 -0500

    Asociar y desasociar permisos a rol

--- routes/permiso.rb
+++ routes/permiso.rb
@@ -44,10 +44,25 @@ class MyApp < Sinatra::Base
 			end
 	  end
     if error == false
-      {:tipo_mensaje => 'success', :mensaje => ['Se ha registrado los cambios en los permisos', array_nuevos]}.to_json
+      return {:tipo_mensaje => 'success', :mensaje => ['Se ha registrado los cambios en los permisos', array_nuevos]}.to_json
     else
       status 500
-      {:tipo_mensaje => 'error', :mensaje => ['Se ha producido un error en guardar la tabla de permisos', e.message]}.to_json
+      return {:tipo_mensaje => 'error', :mensaje => ['Se ha producido un error en guardar la tabla de permisos', e.message]}.to_json
     end
   end
+
+  get '/permiso/listar_asociados/:sistema_id/:rol_id' do
+		DB.fetch('
+  		SELECT T.id AS id, T.nombre AS nombre, (CASE WHEN (P.existe = 1) THEN 1 ELSE 0 END) AS existe, T.llave AS llave FROM
+  		(
+  			SELECT id, nombre, llave, 0 AS existe FROM permisos WHERE sistema_id = ' + params[:sistema_id] + '
+  		) T
+  		LEFT JOIN
+  		(
+  			SELECT P.id, P.nombre,  P.llave, 1 AS existe  FROM permisos P
+  			INNER JOIN roles_permisos RP ON P.id = RP.permiso_id
+  			WHERE RP.rol_id =  ' + params[:rol_id] + '
+  		) P
+  		ON T.id = P.id').to_a.to_json
+	end
 end
--- routes/rol.rb
+++ routes/rol.rb
@@ -43,10 +43,47 @@ class MyApp < Sinatra::Base
       end
     end
     if error == false
-      {:tipo_mensaje => 'success', :mensaje => ['Se ha registrado los cambios en los roles', array_nuevos]}.to_json
+      return {:tipo_mensaje => 'success', :mensaje => ['Se ha registrado los cambios en los roles', array_nuevos]}.to_json
     else
       status 500
-      {:tipo_mensaje => 'error', :mensaje => ['Se ha producido un error en guardar la tabla de roles', e.message]}.to_json
+      return {:tipo_mensaje => 'error', :mensaje => ['Se ha producido un error en guardar la tabla de roles', e.message]}.to_json
+    end
+  end
+
+  post '/rol/asociar_permisos' do
+    data = JSON.parse(params[:data])
+    nuevos = data['nuevos']
+    editados = data['editados']
+    eliminados = data['eliminados']
+    usuario_id = data['extra']
+    rol_id = data['extra']['id_rol']
+    rpta = []
+    error = false
+    execption = nil
+    DB.transaction do
+      begin
+        if nuevos.length != 0
+          nuevos.each do |nuevo|
+            n = RolPermiso.new(:permiso_id => nuevo['id'], :rol_id => rol_id)
+            n.save
+          end
+        end
+        if eliminados.length != 0
+          eliminados.each do |eliminado|
+            RolPermiso.where(:permiso_id => eliminado, :rol_id => rol_id).delete
+          end
+        end
+      rescue Exception => e
+        Sequel::Rollback
+        error = true
+        execption = e
+      end
+    end
+    if error == false
+      return {:tipo_mensaje => 'success', :mensaje => ['Se ha registrado la asociación/deasociación de los permisos al rol', []]}.to_json
+    else
+      status 500
+      return {:tipo_mensaje => 'error', :mensaje => ['Ha ocurrido un error en registrar la asociación/deasociación de los permisos al rol', e.message]}.to_json
     end
   end
 end
