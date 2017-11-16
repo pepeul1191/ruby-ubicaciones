fb6887a7309e275f76cf6a79abdd707cfecb4ee9
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-15 22:52:17 -0500

    Listar items

--- routes/item.rb
+++ routes/item.rb
@@ -1,5 +1,5 @@
 class MyApp < Sinatra::Base
-    get '/item/listar/:subtitulo_id' do
-        Item.select(:id, :nombre, :url).where(:subtitulo_id => params['subtitulo_id']).all().to_a.to_json
-    end
+  get '/item/listar/:subtitulo_id' do
+    Item.select(:id, :nombre, :url).where(:subtitulo_id => params['subtitulo_id']).all().to_a.to_json
+  end
 end
