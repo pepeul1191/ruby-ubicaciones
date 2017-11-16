a12791b8fc412f8b881fcd9fc08d78afd2f5bdd7
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-16 07:44:30 -0500

    Listar usuarios

--- routes/usuario.rb
+++ routes/usuario.rb
@@ -1,20 +1,20 @@
 class MyApp < Sinatra::Base
-    get '/usuario/listar' do
-		DB.fetch('
-        	SELECT U.id AS id, U.usuario AS usuario, A.momento AS momento, U.correo AS correo 
-        	FROM usuarios U INNER JOIN accesos A ON U.id = A.usuario_id GROUP BY U.usuario ORDER BY U.id').to_a.to_json
-    end
+  get '/usuario/listar' do
+    DB.fetch('
+      SELECT U.id AS id, U.usuario AS usuario, A.momento AS momento, U.correo AS correo
+      FROM usuarios U INNER JOIN accesos A ON U.id = A.usuario_id GROUP BY U.usuario ORDER BY U.id').to_a.to_json
+  end
 
-    post '/usuario/validar' do
-  		rpta = Usuario.where(:usuario => params['usuario'], :contrasenia => params['contrasenia']).count()
-    	if rpta == 1
-    		usuario_id = Usuario.select(:id).where(:usuario => params['usuario'], :contrasenia => params['contrasenia']).first().id
-			Acceso.new(:usuario_id => usuario_id, :momento => Time.now).save
-    	end
-    	rpta.to_s
+  post '/usuario/validar' do
+    rpta = Usuario.where(:usuario => params['usuario'], :contrasenia => params['contrasenia']).count()
+    if rpta == 1
+      usuario_id = Usuario.select(:id).where(:usuario => params['usuario'], :contrasenia => params['contrasenia']).first().id
+      Acceso.new(:usuario_id => usuario_id, :momento => Time.now).save
     end
+    rpta.to_s
+  end
 
-    get '/usuario/listar_accesos/:usuario_id' do
-    	Acceso.select(:id, :momento).where(:usuario_id => params['usuario_id']).all().to_a.to_json
-    end
+  get '/usuario/listar_accesos/:usuario_id' do
+    Acceso.select(:id, :momento).where(:usuario_id => params['usuario_id']).all().to_a.to_json
+  end
 end
